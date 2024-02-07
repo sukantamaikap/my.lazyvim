19e141e70766021f0bed88f02f4520436b458ad7
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-02-07 22:02:01 +1100

    added harpoon

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
@@ -27,6 +27,74 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.project" },
+    {
+      "theprimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("harpoon"):setup()
+      end,
+      keys = {
+        {
+          "<leader>A",
+          function()
+            require("harpoon"):list():append()
+          end,
+          desc = "harpoon file",
+        },
+        {
+          "<leader>a",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "harpoon quick menu",
+        },
+        {
+          "<leader>1",
+          function()
+            require("harpoon"):list():select(1)
+          end,
+          desc = "harpoon to file 1",
+        },
+        {
+          "<leader>2",
+          function()
+            require("harpoon"):list():select(2)
+          end,
+          desc = "harpoon to file 2",
+        },
+        {
+          "<leader>3",
+          function()
+            require("harpoon"):list():select(3)
+          end,
+          desc = "harpoon to file 3",
+        },
+        {
+          "<leader>4",
+          function()
+            require("harpoon"):list():select(4)
+          end,
+          desc = "harpoon to file 4",
+        },
+        {
+          "<leader>5",
+          function()
+            require("harpoon"):list():select(5)
+          end,
+          desc = "harpoon to file 5",
+        },
+      },
+    },
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    "nvim-telescope/telescope-symbols.nvim",
+    "ThePrimeagen/harpoon",
+
+    -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
+    "navarasu/onedark.nvim", -- Theme inspired by Atom
+    "nvim-lualine/lualine.nvim",
     -- import/override with your plugins
     { import = "plugins" },
   },

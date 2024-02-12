818f9da063449c765693fee5044e47fd1534ad62
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-02-12 20:17:05 +1100

    updated default theme to onelight

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,7 @@ vim.o.termguicolors = true
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "catppuccin-frappe" } },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "onelight" } },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },

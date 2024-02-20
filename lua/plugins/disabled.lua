44ba96416e42a637316eeed32652fb64d173f744
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-02-20 21:58:21 +1100

    disabled copoilot and enabled gen.nvim for local LLMs

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "jose-elias-alvarez/null-ls", enabled = false },
   -- Disabled headlines.nvim to resolve the h* elements highlight in md file
   { "lukas-reineke/headlines.nvim", enabled = false },
+  { "lazyvim.plugins.extras.coding.copilot", enabled = false },
 }

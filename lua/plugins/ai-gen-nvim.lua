e47b51306fa0ec239873926c730f855f3dc58d46
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-02-20 22:24:48 +1100

    added gen nvim

--- lua/plugins/ai-gen-nvim.lua
+++ lua/plugins/ai-gen-nvim.lua
@@ -2,7 +2,7 @@ return {
   "David-Kunz/gen.nvim",
   opts = {
     model = "codellama", -- The default model to use.
-    display_mode = "float", -- The display mode. Can be "float" or "split".
+    display_mode = "split", -- The display mode. Can be "float" or "split".
     show_prompt = false, -- Shows the Prompt submitted to Ollama.
     show_model = true, -- Displays which model you are using at the beginning of your chat session.
     no_auto_close = false, -- Never closes the window automatically.

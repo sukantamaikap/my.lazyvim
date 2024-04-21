eede021f589e0f688e767ca8f965256d21769a60
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-04-21 19:56:55 +1000

    disabled copilot and copilot for office

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,7 @@
 return {
   { "jose-elias-alvarez/null-ls", enabled = false },
+  { "CopilotC-Nvim/CopilotChat.nvim", enabled = false },
+  { "lazyvim.plugins.extras.coding.copilot", enabled = false },
   -- Disabled headlines.nvim to resolve the h* elements highlight in md file
   { "lukas-reineke/headlines.nvim", enabled = false },
 }

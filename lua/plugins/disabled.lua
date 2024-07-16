91609e8e3f65dbc791e23bdbec7794376aafb830
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-07-17 07:42:29 +1000

    Disabled gen-nvim and enabled back copilot and copilot chat

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,8 @@
 return {
   { "jose-elias-alvarez/null-ls", enabled = false },
-  { "CopilotC-Nvim/CopilotChat.nvim", enabled = false },
-  { "lazyvim.plugins.extras.coding.copilot", enabled = false },
+  -- { "CopilotC-Nvim/CopilotChat.nvim", enabled = false },
+  -- { "lazyvim.plugins.extras.coding.copilot", enabled = false },
   -- Disabled headlines.nvim to resolve the h* elements highlight in md file
   { "lukas-reineke/headlines.nvim", enabled = false },
+  { "David-Kunz/gen.nvim", enabled = false },
 }

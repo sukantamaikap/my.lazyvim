d40435c8ddcd991a3f4701c5cc65ba778ccca97c
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-07-21 09:51:52 +1000

    enabled back gen-nvim and marked it verylazy=true

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   { "jose-elias-alvarez/null-ls", enabled = false },
   -- -- Disabled for home, as I'm using Copilot
-  { "David-Kunz/gen.nvim", enabled = false },
+  -- { "David-Kunz/gen.nvim", enabled = false },
   -- Disabled headlines.nvim to resolve the h* elements highlight in md file
   { "lukas-reineke/headlines.nvim", enabled = false },
 }

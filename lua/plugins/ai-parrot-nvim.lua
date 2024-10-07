77bd37395faf97eb0eaf4fc008a3965e10bb3334
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-10-07 22:39:07 +1100

    added parrot config

---
+++ lua/plugins/ai-parrot-nvim.lua
@@ -0,0 +1,9 @@
+return {
+  "frankroeder/parrot.nvim",
+  dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim", "rcarriga/nvim-notify" },
+  opts = {
+    providers = {
+      ollama = {},
+    },
+  },
+}

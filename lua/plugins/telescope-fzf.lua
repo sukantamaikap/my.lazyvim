751aa02134a4c733dc6f8ea95bec918b517e7767
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-11-15 06:02:56 +1100

    (chore): updated config based on lazy changes

--- lua/plugins/telescope-fzf.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  "nvim-telescope/telescope-fzf-native.nvim",
-  build = "make",
-  cond = vim.fn.executable("make") == 1,
-}

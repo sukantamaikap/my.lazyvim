1705a251e6263b8112c4d1501dff3bc428adc764
Sukanta Maikap <sukanta.maikap@gmail.com>
2024-11-15 05:15:03 +1100

    (chore): updated plugins as per changes in lazyvim

--- lua/plugins/ai-avante.lua
+++ lua/plugins/ai-avante.lua
@@ -4,7 +4,33 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    provider = "copilot",
+    provider = "ollama",
+    vendors = {
+      ---@type AvanteProvider
+      ollama = {
+        ["local"] = true,
+        endpoint = "127.0.0.1:11434/v1",
+        model = " qwen2.5:7b-instruct-q6_K",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
